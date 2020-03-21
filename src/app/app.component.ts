fbc4fb983efea070b118d34736c5019b6d67e54f
Zoechbauer <hans.zoechbauer@aon.at>
2020-03-21 19:24:51 +0100

    Custom pipe: create a filter pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,7 +32,14 @@ export class AppComponent {
       started: new Date(15, 1, 2017)
     }
   ];
-  getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
+  filteredStatus = '';
+
+  getStatusClasses(server: {
+    instanceType: string;
+    name: string;
+    status: string;
+    started: Date;
+  }) {
     return {
       'list-group-item-success': server.status === 'stable',
       'list-group-item-warning': server.status === 'offline',
---
+++ src/app/filter.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+  transform(value: any, filterString: string, propName: string): any {
+    if (value.length === 0 || filterString === '') {
+      return value;
+    }
+    const results = [];
+    for (const item of value) {
+      if (item[propName] === filterString) {
+        results.push(item);
+      }
+    }
+    return results;
+  }
+}
